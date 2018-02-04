178bd6d3522b61ba17de57acc2e81a963a79e029
Andreas Wong <andreaswong770@gmail.com>
2018-02-05 02:48:57 +0800

    master

--- main.go
+++ main.go
@@ -1,17 +1,59 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
+
+	"github.com/andreaswong/eatigo-pr-cycle/permutation"
+	"github.com/joho/godotenv"
+	"github.com/mitchellh/mapstructure"
 )
 
 func main() {
+	envs, err := godotenv.Read()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	config := &Config{}
+	err = mapstructure.Decode(envs, config)
+	if err != nil {
+		log.Fatal("Unable to decode from .env to %v, error=%v", config, err)
+		panic(nil)
+	}
+
 	port := os.Getenv("PORT")
-	http.ListenAndServe(":"+port, &DefaultHandler{})
+	http.ListenAndServe(":"+port, NewDefaultHandler(config))
 }
 
-type DefaultHandler struct{}
+type DefaultHandler struct {
+	config *Config
+}
+
+func NewDefaultHandler(config *Config) *DefaultHandler {
+	return &DefaultHandler{
+		config: config,
+	}
+}
 
 func (h *DefaultHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(r.URL.Query().Get("s")))
+	now := time.Now().UTC()
+	weekday := int(now.Weekday())
+
+	objJson := h.config.ObjectsJson
+	engineers := []string{}
+	if err := json.Unmarshal([]byte(objJson), engineers); err != nil {
+		panic("Unable to unmarshal OBJECTS_JSON from env (is it a json array string?)")
+	}
+
+	index := weekday % len(engineers)
+	w.Write([]byte(strings.Join(permutation.Permute(engineers)[index], " reviews PR of ")))
+}
+
+type Config struct {
+	ObjectsJson string `mapstructure:"OBJECTS_JSON"`
 }
---
+++ permutation/permute.go
@@ -0,0 +1,31 @@
+package permutation
+
+// Permute implements https://en.wikipedia.org/wiki/Heap%27s_algorithm
+func Permute(arr []string) [][]string {
+	var helper func([]string, int)
+	res := [][]string{}
+
+	helper = func(arr []string, n int){
+		if n == 1{
+			tmp := make([]string, len(arr))
+			copy(tmp, arr)
+			res = append(res, tmp)
+		} else {
+			for i := 0; i < n; i++{
+				helper(arr, n - 1)
+				if n % 2 == 1{
+					tmp := arr[i]
+					arr[i] = arr[n - 1]
+					arr[n - 1] = tmp
+				} else {
+					tmp := arr[0]
+					arr[0] = arr[n - 1]
+					arr[n - 1] = tmp
+				}
+			}
+		}
+	}
+
+	helper(arr, len(arr))
+	return res
+}
