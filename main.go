8f25591cfb15aa6edeba614a4213d5923026082d
Andreas Wong <andreaswong770@gmail.com>
2018-02-04 23:08:22 +0800

    master goimports

--- main.go
+++ main.go
@@ -7,12 +7,11 @@ import (
 
 func main() {
 	port := os.Getenv("PORT")
-	http.ListenAndServe(":" + port, &DefaultHandler{})
+	http.ListenAndServe(":"+port, &DefaultHandler{})
 }
 
-type DefaultHandler struct {}
+type DefaultHandler struct{}
 
 func (h *DefaultHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(r.URL.Query().Get("s")))
 }
-
